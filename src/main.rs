e2a99f05c298152f1b85579457a8e253802f2954
Xiaonuo Gantan <xgantan@execvision.io>
2023-10-10 10:29:53 -0400

    Response with content

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -12,11 +12,11 @@ fn main() {
                 println!("accepted new connection");
                 let mut buf = [0; 512];
                 _stream.read(&mut buf).unwrap();
-                let path = handle(&mut buf);
+                let path = get_path(&mut buf);
                 if path == "/" {
                     _stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
                 } else {
-                    _stream.write_all(b"HTTP/1.1 404 NotFound\r\n\r\n").unwrap();
+                    handle_echo(path, &mut _stream);
                 }
             }
             Err(e) => {
@@ -26,8 +26,27 @@ fn main() {
     }
 }
 
-fn handle(buf: &mut [u8]) -> String {
+fn get_path(buf: &mut [u8]) -> String {
     let data = String::from_utf8(buf.to_vec()).unwrap();
     let first_line = data.split_once("\r\n\r\n").unwrap().0;
     return first_line.split(" ").nth(1).unwrap().to_string();
+}
+
+fn handle_echo(path: String, stream: &mut TcpStream) {
+    let all: Vec<&str> = path.split("/").collect(); // First element is empty
+    let segments = &all[1..];
+    if segments.len() >= 2 {
+        if segments[0] == "echo" {
+            let echo = segments[1..].join("/");
+            let resp = format!("\
+HTTP/1.1 200 OK\r\n\
+Content-Type: text/plain\r\n\
+Content-Length: {}\r\n\
+\r\n\
+{}\r\n", echo.len(), echo);
+            stream.write_all(resp.as_bytes()).unwrap();
+            return;
+        }
+    }
+    stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n").unwrap();
 }
