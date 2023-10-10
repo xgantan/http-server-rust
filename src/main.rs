8612da99dd804caeeb2e743e4540d6ff940a45bb
Xiaonuo Gantan <xgantan@execvision.io>
2023-10-10 10:39:38 -0400

    Parse the User-Agent header

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,8 @@ fn main() {
                 if path == "/" {
                     _stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
                 } else {
-                    handle_echo(path, &mut _stream);
+                    let data = get_data(&mut buf);
+                    handle(path, data, &mut _stream);
                 }
             }
             Err(e) => {
@@ -28,11 +29,17 @@ fn main() {
 
 fn get_path(buf: &mut [u8]) -> String {
     let data = String::from_utf8(buf.to_vec()).unwrap();
-    let first_line = data.split_once("\r\n\r\n").unwrap().0;
+    let first_line = data.split_once("\r\n").unwrap().0;
     return first_line.split(" ").nth(1).unwrap().to_string();
 }
 
-fn handle_echo(path: String, stream: &mut TcpStream) {
+fn get_data(buf: &mut [u8]) -> String {
+    let data = String::from_utf8(buf.to_vec()).unwrap();
+    let rest = data.split_once("\r\n").unwrap().1;
+    return rest.to_string();
+}
+
+fn handle(path: String, data: String, stream: &mut TcpStream) {
     let all: Vec<&str> = path.split("/").collect(); // First element is empty
     let segments = &all[1..];
     if segments.len() >= 2 {
@@ -45,6 +52,22 @@ Content-Length: {}\r\n\
 \r\n\
 {}\r\n", echo.len(), echo);
             stream.write_all(resp.as_bytes()).unwrap();
+            return;
+        }
+    } else {
+        if segments[0] == "user-agent" {
+            data.split("\r\n").for_each(|line| {
+                if line.starts_with("User-Agent: ") {
+                    let ua = line.split_once("User-Agent: ").unwrap().1;
+                    let resp = format!("\
+HTTP/1.1 200 OK\r\n\
+Content-Type: text/plain\r\n\
+Content-Length: {}\r\n\
+\r\n\
+{}\r\n", ua.len(), ua);
+                    stream.write_all(resp.as_bytes()).unwrap();
+                }
+            });
             return;
         }
     }
