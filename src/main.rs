8443a6c5df1174083de0c5464686aa2315f58753
Xiaonuo Gantan <xgantan@execvision.io>
2023-10-10 11:43:18 -0400

    Implement Get-a-File

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::fs;
+use std::path::Path;
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     println!("Logs from your program will appear here!");
-
+    let args: Vec<String> = std::env::args().collect();
+    let mut target_dir: Option<String> = None;
+    if let Some(index) = args.iter().position(|arg| arg == "--directory") {
+        target_dir = Option::from(args[index + 1].clone());
+    }
     let listener = TcpListener::bind("127.0.0.1:4221").await?;
     loop {
+        let dir = target_dir.clone();
         let (mut stream, _) = listener.accept().await?;
         tokio::task::spawn(async move {
             println!("accepted new connection");
@@ -17,7 +25,7 @@ async fn main() -> anyhow::Result<()> {
                 stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await?;
             } else {
                 let data = get_data(&mut buf);
-                handle(path, data, &mut stream).await?;
+                handle(path, data, &mut stream, dir).await?;
             }
             Ok::<(), anyhow::Error>(())
         });
@@ -36,39 +44,55 @@ fn get_data(buf: &mut [u8]) -> String {
     return rest.to_string();
 }
 
-async fn handle(path: String, data: String, stream: &mut TcpStream) -> anyhow::Result<()> {
+async fn handle(path: String, data: String, stream: &mut TcpStream, target_dir: Option<String>) -> anyhow::Result<()> {
     let all: Vec<&str> = path.split("/").collect(); // First element is empty
     let segments = &all[1..];
-    if segments.len() >= 2 {
-        if segments[0] == "echo" {
-            let echo = segments[1..].join("/");
-            let resp = format!("\
+    if segments[0] == "echo" {
+        let echo = segments[1..].join("/");
+        let resp = format!("\
 HTTP/1.1 200 OK\r\n\
 Content-Type: text/plain\r\n\
 Content-Length: {}\r\n\
 \r\n\
 {}\r\n", echo.len(), echo);
-            stream.write_all(resp.as_bytes()).await?;
-        } else {
-            stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n").await?;
-        }
-    } else {
-        if segments[0] == "user-agent" {
-            for line in data.split("\r\n") {
-                if line.starts_with("User-Agent: ") {
-                    let ua = line.split_once("User-Agent: ").unwrap().1;
-                    let resp = format!("\
+        stream.write_all(resp.as_bytes()).await?;
+    } else if segments[0] == "user-agent" {
+        for line in data.split("\r\n") {
+            if line.starts_with("User-Agent: ") {
+                let ua = line.split_once("User-Agent: ").unwrap().1;
+                let resp = format!("\
 HTTP/1.1 200 OK\r\n\
 Content-Type: text/plain\r\n\
 Content-Length: {}\r\n\
 \r\n\
 {}\r\n", ua.len(), ua);
+                stream.write_all(resp.as_bytes()).await?;
+            }
+        };
+    } else if segments[0] == "files" {
+        let filename = segments[1..].join("/");
+        let filepath = &format!("{}/{}", target_dir.unwrap(), filename);
+        let path = Path::new(filepath);
+        if path.exists() {
+            match fs::read_to_string(path) {
+                Ok(content) => {
+                    let resp = format!("\
+HTTP/1.1 200 OK\r\n\
+Content-Type: application/octet-stream\r\n\
+Content-Length: {}\r\n\
+\r\n\
+{}", content.len(), content);
                     stream.write_all(resp.as_bytes()).await?;
                 }
-            };
+                Err(_) => {
+                    stream.write_all(b"HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n").await?;
+                }
+            }
         } else {
             stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n").await?;
         }
+    } else {
+        stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n").await?;
     }
     Ok(())
 }
